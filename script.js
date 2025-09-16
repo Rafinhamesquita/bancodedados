ad786429f2dbfaab2e9b2a02ff97eae6ce322b70
Rafaela Mesquita <rafaela.mesquita@escola.pr.gov.br>
2025-09-15 23:15:44 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Simulação simples de carrinho (não salva ainda)
-let carrinho = [];
-
-function adicionarCarrinho(produto, preco) {
-  carrinho.push({ produto, preco });
-  alert(produto + " adicionado ao carrinho!");
-}
